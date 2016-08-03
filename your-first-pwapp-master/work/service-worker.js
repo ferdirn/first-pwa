9af3b79e5953995ac6fd626d863a802fee8a6934
Ferdi Ramdhon <ferdirn@gmail.com>
2016-08-03 17:10:28 +0700

    Use service workers to cache the forecast data

--- your-first-pwapp-master/work/service-worker.js
+++ your-first-pwapp-master/work/service-worker.js
@@ -1,7 +1,10 @@
-var cacheName = 'weatherPWA-step-6-1';
+var cacheName = 'weatherPWA-step-6-1-0';
+var dataCacheName = 'weatherData-v1';
+
 var filesToCache = [
   '/',
   '/index.html',
+  '/favicon.ico',
   '/scripts/app.js',
   '/styles/inline.css',
   '/images/clear.png',
@@ -45,9 +48,23 @@ self.addEventListener('activate', function(e) {
 
 self.addEventListener('fetch', function(e) {
   console.log('[ServiceWorker] Fetch', e.request.url);
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+  var dataUrl = 'https://publicdata-weather.firebaseio.com/';
+  if (e.request.url.indexOf(dataUrl) === 0) {
+    e.respondWith(
+      fetch(e.request)
+        .then(function(response) {
+          return caches.open(dataCacheName).then(function(cache) {
+            cache.put(e.request.url, response.clone());
+            console.log('[ServiceWorker] Fetched & Cached Data');
+            return response;
+          });
+        })
+    );
+  } else {
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+    );
+  }
 });
